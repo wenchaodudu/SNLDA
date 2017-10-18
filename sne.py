5813d2511b114f900c0098d17c39c0de8eb1b023
Wenchao Du <wdu@localhost.localdomain>
2017-10-18 17:05:47 -0400

    template for admm

--- sne.py
+++ sne.py
@@ -2,15 +2,17 @@ import numpy as np
 from snlda import DOC_NUM, TOPIC_NUM
 
 '''
-    theta: D * TOPIC_NUM, document topic vectors
+    theta_1: D * TOPIC_NUM, document topic vectors returned from f
+    theta_2: D * TOPIC_NUM, document topic vectors returned from g in the last iteration
     W: D * D adjacency matrix, 1 if in same category, -1 if in different category, 0 if unknown
     lambda: scalar, parameter of Gaussian prior
     u: D * TOPIC_NUM, penalty term of admm, 
     rho: scalar, penalty coeffcient of admm
+    it: scalar, iteration count
 
     return: updated theta
 '''
 
-def update_theta(theta, W, lamba, u, rho):
+def update_theta(theta_1, theta_2, W, lamba, u, rho, it):
     pass
 
--- snlda.py
+++ snlda.py
@@ -1,6 +1,29 @@
 import numpy as np
+import itertools
 from sne import update_theta
 from lda import update_variables
 
-def admm():
-    pass
+TOPIC_NUM = 0
+DOC_NUM = 0
+VOCAB_SIZE = 0
+prior_std = np.sqrt(2) / 2
+
+def admm(X, W, rho, iter_num):
+    # initialize variables
+    theta_1 = np.random.normal(scale=prior_std, size=(DOC_NUM, TOPIC_NUM))
+    theta_2 = np.copy(theta_1)
+    q_z = [dict() for x in range(DOC_NUM)]
+    dirichlet_prior = np.full(TOPIC_NUM, 2)
+    for x in range(DOC_NUM):
+        for y in range(VOCAB_SIZE):
+            if X[x, y]:
+                q_z[x][y] = np.random.dirichlet(dirichlet_prior, 1)[0]
+    beta = np.random.dirichlet(np.full(VOCAB_SIZE, 2), TOPIC_NUM)
+    u = np.zeros((DOC_NUM, TOPIC_NUM))
+
+    for it in range(iter_num):
+        theta_1, q_z, beta = update_variables(X, theta_1, theta_2, q_z, beta, rho, u, it)
+        theta_2 = update_theta(theta_1, theta_2, W, 1 / (2 * prior_std**2), u, rho, it)
+        u += (theta_1 - theta_2)
+        return (theta_1 + theta_2) / 2
+
