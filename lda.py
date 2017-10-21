35e57aab4d6a53149b207f9fb834bf48f0ac4864
w8du <duwc2013@gmail.com>
2017-10-21 15:07:56 -0400

    finish variational update

--- lda.py
+++ lda.py
@@ -10,6 +10,7 @@ def exp_proportion(theta):
 def update_variables(X, theta_1, theta_2, q_z, beta, rho, u, it):
     DOC_NUM, TOPIC_NUM = X.shape
     for itt in range(iter_num):
+        new_beta = np.ones(beta.shape)
         for x in range(DOC_NUM):
             # update theta
             Ez = sum((digamma(q_z[x][w]) - digamma(np.sum(q_z[x][w]))) * X[x,w] for w in q_z[x])
@@ -20,3 +21,13 @@ def update_variables(X, theta_1, theta_2, q_z, beta, rho, u, it):
             eta_hessian = np.asmatrix(pi).T * np.asmatrix(pi) - np.diag(pi)
             Sigma = eta_hessian * np.sum(Ez) - rho
             Eq = theta_1[x] - np.log(np.sum(np.exp(theta_1[x]))) + 1 / 2 * np.trace(eta_hessian * Sigma)
+            for w in q_z:
+                qz = Eq + np.log(beta[:, w])
+                qz = np.exp(qz)
+                q_z[w] = qz / np.sum(qz)
+                new_beta[ += q_z[w] * X[x, w]
+
+        # update beta
+        new_beta /= np.sum(new_beta, axis=1)[:, np.newaxis]
+    return theta_1, q_z, beta
+
