9c8ff3e2a5c162321c46e69cc911469f0f38a301
yuelicmu <yuelipku1996@gmail.com>
2017-10-23 21:06:53 -0400

    modify sne

--- sne.py
+++ sne.py
@@ -5,23 +5,25 @@ import random
 # update_theta(theta_1, theta_2, W, 1 / (2 * prior_std**2), u, rho, it)
 '''
     theta: D * TOPIC_NUM, document topic vectors
-    W: D * D adjacency matrix, 1 if in same category, -1 if in different category, 0 if unknown
+    theta_2: D * TOPIC_NUM, the starting point for optimization
+    # W: D * D adjacency matrix, 1 if in same category, -1 if in different category, 0 if unknown
     lambda: scalar, parameter of Gaussian prior
     u: D * TOPIC_NUM, penalty term of admm, 
     rho: scalar, penalty coeffcient of admm
+    C: a list representing the category of each data point. e.g.: ['1','2',None,'1']
 
     return: updated theta
 '''
 
-
-def update_theta(theta0, starting, W, C, lamba, u, rho, step=0.001):
+def update_theta(theta0, starting, C, lamba, u, rho, step=0.001):
     # W: a dict representing the category
     # C: a list representing the category
-    D = len(C)
+    # processing W and C
+
     theta = starting
-    for _ in range(1000):
+    for _ in range(10):
         theta_old = theta.copy()
-        ite = list(range(D))
+        ite = np.where(C!=-1)[0]
         random.shuffle(ite)
         for i in ite:
             dir = - gradient(i, C[i], theta, theta0, W, lamba, u, rho)
@@ -41,7 +43,7 @@ def objective(theta, theta0, W, C, lamda, u, rho):
     D = len(C)
     y0 = np.sum(np.sum(np.linalg.norm(theta[i, :] - theta[j, :]) ** 2 for i in l for j in l) for l in list(W.values()))
     def S(i):
-        category = set(W.keys()) - set(C[i])
+        category = set(W.keys()) - set([C[i]])
         s = np.sum(np.sum(np.exp(-np.linalg.norm(theta[i, :] - theta[j, :]) ** 2) for j in W[l]) for l in category)
         return (np.log(s))
 
@@ -53,15 +55,14 @@ def objective(theta, theta0, W, C, lamda, u, rho):
 def gradient(i, ci, theta, theta0, W, lamda, u, rho):
     # W: a dict representing the category
     # ci: the category of i-th point
-    category = set(W.keys()) - set(ci)
+    category = set(W.keys()) - set([ci])
     M = {}
     for d in category:
         M[d] = np.mean(W[d])
     g0 = 4 * np.sum((theta[i, :] - theta[j, :]) for j in W[ci])
-    # print(list(M.values()))
-    # print(len(list(M.values())))
-    S = np.sum(len(l) * np.exp(-np.linalg.norm(theta[i, :], M[l])) for l in M.keys())
-    g1 = np.sum(len(l) * np.exp(-np.linalg.norm(theta[i, :], M[l])) * 2 * (theta[i, :] - M[l]) for l in M.keys()) / S
+    # print(M)
+    S = np.sum(len(W[l]) * np.exp(-np.linalg.norm(theta[i, :], M[l])) for l in M.keys())
+    g1 = np.sum(len(W[l]) * np.exp(-np.linalg.norm(theta[i, :], M[l])) * 2 * (theta[i, :] - M[l]) for l in M.keys()) / S
     g2 = lamda * 2 * theta[i, :] + rho * (theta[i, :] - theta0[i, :] - u[i, :])
     return (g0 + g1 + g2)
 
@@ -70,12 +71,16 @@ if __name__ == '__main__':
     # update_theta(theta0, W, lamba, u, rho)
     theta0 = np.random.rand(100, 20)
     theta = np.random.rand(100, 20)
-    W = {'1':list(range(50)),'2':list(range(50,100))}
-    C = list('1'*50+'2'*50)
+    W = {1:list(range(40)), 2:list(range(40,80))}
+    C = np.full(100,0)
+    for i in range(40):
+        C[i] = 1
+    for i in range(40,80):
+        C[i] = 2
+    for i in range(80,100):
+        C[i] = -1
     lamba = 100
     u = np.random.rand(100, 20)
     rho = 0.01
-    G = gradient(0,'1',theta,theta0, W, lamba, u, rho)
-    N = update_theta(theta0, theta0, W,C, lamba, u, rho)
-    # s1 = np.sum(np.sum(np.linalg.norm(theta[i,:] - theta[j,:]) ** 2 for i in l for j in l) for l in list(W.values()))
-    # print(N)
+    G = gradient(0,1,theta, theta0, W, lamba, u, rho)
+    N = update_theta(theta0, theta0, C, lamba, u, rho)
